f39a775e349ebbc32a7d3680f508b28f42a74db8
Ulysse Clement <ulysse@ulyssepc.home>
2023-06-16 09:45:36 +0200

    push

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ulysse <ulysse@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 15:28:29 by uclement          #+#    #+#             */
-/*   Updated: 2023/06/13 16:11:39 by ulysse           ###   ########.fr       */
+/*   Updated: 2023/06/16 09:42:38 by ulysse           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,61 +40,48 @@ typedef struct s_stack
 	struct s_stack	*next;
 }					t_stack;
 
-void	pair_swap(t_stack **stack);
-void	print_stack(t_stack *a, t_stack *b);
 void	to_list_a(char **argv, int argc, t_stack **stack_a);
 void	error_exit(void);
+t_stack	*create_stack(int content);
+
+void	pair_swap(t_stack **stack);
 void	push(t_stack **receiver, t_stack **sender);
+void	push_suite(t_stack **receiver, t_stack *curr);
 void	rotate(t_stack **stack_a);
 void	reverse(t_stack	**stack);
 
-void	free_lst(t_stack	**lst);
-
-int		doublon(t_stack	*lst);
-
-void	move(t_stack **stack_a, t_stack **stack_b, int call);
-void	move_2(t_stack **stack_a, t_stack **stack_b, int call);
-void	print_move(int call);
-
-void	index_maker(t_stack **stack);
-void	median_sort(t_stack **a, t_stack **b);
-void	three(t_stack **a, t_stack **b);
-void	five(t_stack **a, t_stack **b);
-void	last_sort(t_stack **stack, t_stack **b);
-
-void	find_pos(t_stack **a, t_stack **b);
-void	sort(t_stack **a, t_stack **b, int count);
+void	iter_index(t_stack **a, t_stack **b);
+void	index_find_pos(t_stack **a, t_stack **b);
+void	index_sort(t_stack **a, t_stack **b, int count);
 
 void	compteur(t_instruct *count, int call);
 void	calculateur(t_stack **b, int call);
 void	search_index_min(t_stack **a, t_stack **b);
-void	iter_index(t_stack **a, t_stack **b);
-void	index_find_pos(t_stack **a, t_stack **b);
-void	index_sort(t_stack **a, t_stack **b, int count);
 void	sort_b_stack(t_stack **a, t_stack **b, int min);
-
-void	double_move(t_stack **stack_a, t_stack **stack_b, int call);
-
-int		is_sort(t_stack **a);
-
-int	ps_atoi(const char *nptr);
-int	ps_atoi_suite(const char *nptr, int i, int neg);
-
-void	push_suite(t_stack **receiver, t_stack *curr);
 int		iter_i_min(t_stack **b, int min);
 
-t_stack	*create_stack(int content);
+void	move(t_stack **stack_a, t_stack **stack_b, int call);
+void	double_move(t_stack **stack_a, t_stack **stack_b, int call);
+void	print_move(int call);
 
-void	median_rest(t_stack **a, t_stack **b);
+int		parsing(t_stack **a);
+int		doublon(t_stack	*lst);
+int		is_sort(t_stack **a);
+int		ps_atoi(const char *nptr);
+int		ps_atoi_suite(const char *nptr, int i, int neg);
 
+void	index_maker(t_stack **stack);
+void	median_sort(t_stack **a, t_stack **b);
+void	three(t_stack **a, t_stack **b);
+
+void	find_pos(t_stack **a, t_stack **b);
+void	sort(t_stack **a, t_stack **b, int count);
+void	last_sort(t_stack **stack, t_stack **b);
 void	last_sort_suite(t_stack **stack, t_stack **b, int count);
 
-int	ps_lstsize(t_stack *lst);
-
+void	free_lst(t_stack	**lst);
 void	ps_lstadd_front(t_stack **lst, t_stack *new);
-
+int		ps_lstsize(t_stack *lst);
 t_stack	*ps_lstlast(t_stack *lst);
-
-int	parsing(t_stack **a);
 
 #endif
